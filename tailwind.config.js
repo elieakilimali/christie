f352df48c6b1c253fc044dc41984677c0e25c5b4
elieaz <elieazdev@gmail.com>
2025-06-01 03:33:45 +0200

    Files restricturation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.{html,js}"],
+  content: [ "./src/**/*.{html,js,jsx,ts,tsx}",],
+  
   theme: {
     extend: {
       colors: {
